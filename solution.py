5a607b7aa0f9d468b5b377dd44b90b10cb35e8c4
Shivansh <shivansh.rawat@enine.school>
2024-02-26 12:33:01 +0530

    modified:   solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 """
 Randomizing data
 """
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 import random
 
 
@@ -9,23 +9,13 @@ def create_assets(p, x):
     """
     Create random asset data
     """
-    today = datetime.today()
+    today = date.today()
     start = today - timedelta(days=x)
+    dates = [start + timedelta(days=i) for i in range(x)]
     data = []
     for i in range(1, p + 1):
-        year = random.choice(range(start.date().year, today.date().year + 1))
-        if year == today.date().year:
-            day = random.choice(range(1, today.date().day + 1))
-            month = random.choice(range(1, today.date().month + 1))
-        elif year == today.date().year:
-            day = random.choice(range(1, 29))
-            month = random.choice(range(start.date().month, 13))
-        else:
-            day = random.choice(range(1, 29))
-            month = random.choice(range(1, 13))
-
         data.append(
-            {"id": i, "purchase_date": datetime(year, month, day).date()},
+            {"id": i, "purchase_date": random.choice(dates)},
         )
     return data
 
@@ -34,24 +24,19 @@ def create_rentals(assets, q):
     """
     Create random rental data
     """
+    today = date.today()
     rentals = []
     dates = {}
     id_count = 1
     while len(rentals) < q:
-        today = datetime.today()
         asset = random.choice(assets)
         a_id = asset["id"]
         s_date = asset["purchase_date"]
         if a_id in dates:
-            s_date = datetime(
-                dates[a_id].year, dates[a_id].month, dates[a_id].day
-            ).date()
-        if s_date.month < 12:
-            e_date = datetime(s_date.year, s_date.month + 1, s_date.day).date()
-        else:
-            e_date = datetime(s_date.year + 1, 1, s_date.day).date()
-        if s_date > today.date():
+            s_date = dates[a_id]
+        if s_date > today:
             continue
+        e_date = s_date + timedelta(days=30)
         rentals.append(
             {"id": id_count, "asset_id": a_id, "start_date": s_date, "end_date": e_date}
         )
